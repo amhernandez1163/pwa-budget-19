5f06882bea58683082b8ce9ee86be9ee7ec52570
Amanda Dell <amhernandez1163@gmail.com>
2022-06-19 18:55:35 -0500

    testing complete - tranactions are being saved in indexDB and added once online

--- public/js/idb.js
+++ public/js/idb.js
@@ -1,13 +1,13 @@
 // create variable to hold db connection
 let db;
-// establish a connection to IndexedDB database called 'pizza_hunt' and set it to version 1
+// establish a connection to IndexedDB database called 'budget-tracker' and set it to version 1
 const request = indexedDB.open("budget-tracker", 1);
 
 // this event will emit if the database version changes (nonexistant to version 1, v1 to v2, etc.)
 request.onupgradeneeded = function (event) {
   // save a reference to the database
   const db = event.target.result;
-  // create an object store (table) called `new_pizza`, set it to have an auto incrementing primary key of sorts
+  // create an object store (table) called `new_transaction`, set it to have an auto incrementing primary key of sorts
   db.createObjectStore("new_transaction", { autoIncrement: true });
 };
 
@@ -16,7 +16,7 @@ request.onsuccess = function (event) {
   // when db is successfully created with its object store (from onupgradedneeded event above) or simply established a connection, save reference to db in global variable
   db = event.target.result;
 
-  // check if app is online, if yes run uploadPizza() function to send all local db data to api
+  // check if app is online, if yes run uploadTransaction() function to send all local db data to api
   if (navigator.onLine) {
     uploadTransaction();
   }
@@ -27,13 +27,13 @@ request.onerror = function (event) {
   console.log(event.target.errorCode);
 };
 
-// This function will be executed if we attempt to submit a new pizza and there's no internet connection
+// This function will be executed if we attempt to submit a new transaction and there's no internet connection
 function saveRecord(record) {
   // open a new transaction with the database with read and write permissions
   const transaction = db.transaction(["new_transaction"], "readwrite");
 
-  // access the object store for `new_pizza`
-  const budgetbjectStore = transaction.objectStore("new_transaction");
+  // access the object store for `new_transaction`
+  const budgetObjectStore = transaction.objectStore("new_transaction");
 
   // add record to your store with add method
   budgetObjectStore.add(record);
@@ -49,12 +49,11 @@ function uploadTransaction() {
   // get all records from store and set to a variable
   const getAll = budgetObjectStore.getAll();
 
-  // more to come...
   // upon a successful .getAll() execution, run this function
   getAll.onsuccess = function () {
     // if there was data in indexedDb's store, let's send it to the api server
     if (getAll.result.length > 0) {
-      fetch("/api/transactions/bulk", {
+      fetch("/api/transaction/bulk", {
         method: "POST",
         body: JSON.stringify(getAll.result),
         headers: {
@@ -69,7 +68,7 @@ function uploadTransaction() {
           }
           // open one more transaction
           const transaction = db.transaction(["new_transaction"], "readwrite");
-          // access the new_pizza object store
+          // access the new_transaction object store
           const budgetObjectStore = transaction.objectStore("new_transaction");
           // clear all items in your store
           budgetObjectStore.clear();
