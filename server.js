da2a504914f32de9f06654c819ad870415120de9
Amanda Dell <amhernandez1163@gmail.com>
2022-06-19 20:24:09 -0500

    deploying

--- server.js
+++ server.js
@@ -4,8 +4,7 @@ const mongoose = require("mongoose");
 const compression = require("compression");
 
 const PORT = process.env.PORT || 3001;
-const MONGODB_URI =
-  process.env.MONGODB_URI || "mongodb://localhost:27017/buget-tracker";
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
 
 const app = express();
 
@@ -19,7 +18,7 @@ app.use(express.static("public"));
 
 mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
-  useUnifiedTopology: true,
+  useFindAndModify: false,
 });
 
 // routes
